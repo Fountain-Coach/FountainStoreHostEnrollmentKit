051a0ca24acd6440bcc7f0ace00a423198a5ee01
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2026-08-24 06:58:27 +0200

    Align kit deployment floor with FountainStore

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "FountainStoreHostEnrollmentKit",
-    platforms: [.macOS(.v14), .iOS(.v17)],
+    platforms: [.macOS(.v13), .iOS(.v16)],
     products: [
         .library(name: "FountainStoreHostEnrollmentKit", targets: ["FountainStoreHostEnrollmentKit"])
     ],
